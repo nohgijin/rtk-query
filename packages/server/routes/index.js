62b85900ee10a1a8d14ce0efa794e19564f4d500
nohgijin <ynda11111@naver.com>
2022-01-25 17:40:47 +0900

    setup server

---
+++ packages/server/routes/index.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var router = express.Router();
+
+let users = [
+  { id: 1, name: "춘식" },
+  { id: 2, name: "찐스" },
+];
+
+router.get("/users", function (req, res, next) {
+  res.json(users);
+});
+
+router.post("/users", function (req, res, next) {
+  users = [...users, req.body];
+  res.json(200);
+});
+
+router.delete("/users/:userId", function (req, res, next) {
+  const userId = Number(req.params.userId);
+  users = users.filter((user) => userId !== user.id);
+  res.json(200);
+});
+
+router.patch("/users/:userId", function (req, res, next) {
+  const userId = Number(req.params.userId);
+
+  users = users.map((user) => {
+    if (userId === user.id) {
+      return {
+        id: userId,
+        name: req.body.name,
+      };
+    }
+
+    return user;
+  });
+
+  res.json(200);
+});
+
+module.exports = router;
